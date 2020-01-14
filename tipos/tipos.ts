3ac6292ae9807dbe876490b635e454a0d0ba6958
Rafael <www.rafadraw@gmail.com>
2020-01-14 16:03:13 -0300

    Tipos Explicitos

--- tipos/tipos.ts
+++ tipos/tipos.ts
@@ -1,15 +1,22 @@
 // string
-let nome = 'joão';
+let nome: string = 'joão';
 console.log(nome);
 // nome = 28;
 
 // numbers
-let idade  = 27;
+let idade: number = 27;
 // idade = 'ana'
 idade = 27.5;
 console.log(idade);
 
 // boolean
-let possuiHobbies = false;
+let possuiHobbies: boolean = false;
 // possuiHobbies = 1;
 console.log(possuiHobbies);
+
+// Tipos explicitos;
+let minhaIdade: number;
+minhaIdade = 27;
+// minhaIdade = 'idade é 27';
+console.log(typeof  minhaIdade);
+
