4623a207852f3dca12a05a3dff7b416b50608ebf
Rafael <www.rafadraw@gmail.com>
2020-01-14 15:54:54 -0300

    Tipos implicitos

---
+++ tipos/tipos.ts
@@ -0,0 +1,15 @@
+// string
+let nome = 'joão';
+console.log(nome);
+// nome = 28;
+
+// numbers
+let idade  = 27;
+// idade = 'ana'
+idade = 27.5;
+console.log(idade);
+
+// boolean
+let possuiHobbies = false;
+// possuiHobbies = 1;
+console.log(possuiHobbies);
