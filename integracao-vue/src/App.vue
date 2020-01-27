89165312d2c20e85f27fb5bb127ccb049b334ca6
Rafael <www.rafadraw@gmail.com>
2020-01-27 16:09:30 -0300

    typeScript + Vue Final

--- integracao-vue/src/App.vue
+++ integracao-vue/src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="app">
-
+		<Contador	:valorInicial="10"/>
+		<ContadorClasse :valorInicial="100" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 
+import Contador from "@/components/Contador.vue";
+import ContadorClasse from "@/components/ContadorClasse.vue";
 
 @Component({
   components: {
-
+  	Contador,
+		ContadorClasse
   },
 })
 export default class App extends Vue {}
